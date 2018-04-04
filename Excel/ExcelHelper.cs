229edc3417aa2c5150bbad8588fda93b50d685cc
eherrera <ernestoh2006@gmail.com>
2018-04-03 23:28:19 -0400

    Excel loader

---
+++ Excel/ExcelHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LinealCutOptimizer.Core.Helpers
+{
+    public class ExcelLoader
+    {
+        public static IList<T> Load<T>(string fileName)
+        {
+            var book = new LinqToExcel.ExcelQueryFactory(fileName);
+
+            var query =
+                from row in book.Worksheet<T>(0)
+                select row;
+            return query.ToList();
+        }
+    }
+}
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FullLinearCutSolution.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Librería núcleo de la solución de corte lineal.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/eherrera")]
 [assembly: AssemblyProduct("FullLinearCutSolution.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
